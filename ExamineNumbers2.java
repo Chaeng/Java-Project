417befafc8b4a9004dce93caa56fc0011f6922b4
Chaeng <chaeng4@gmail.com>
2018-01-24 20:13:44 -0800

    First commit

---
+++ ExamineNumbers2.java
@@ -0,0 +1,15 @@
+
+import java.util.*;
+
+public class ExamineNumbers2{
+	public static void main(String[] args){
+
+		Scanner console = new Scanner(System.in);
+		
+		System.out.println("How many?");
+		int totalNumber = console.nextInt();
+
+		System.out.println("You enter " + totalNumber + ".");
+
+	}
+}
